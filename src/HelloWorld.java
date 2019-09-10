e3307434ccc9a0b9ebc21db6680af9832fbd7d11
Ekaterina-C <innocentdaiver@gmail.com>
2019-09-10 01:57:49 -0400

    password check

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// New Updates
 		System.out.println("Hello github!");
+		System.out.println("Hello there!!!");
+		System.out.println("How is everything?");
 		
 	}
 
